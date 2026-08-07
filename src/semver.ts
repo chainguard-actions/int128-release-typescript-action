e8cfc94baa2243f1694e9d7669f8e3dd49d6c5d2
actionchain <actionchain@chainguard.dev>
2026-08-07 21:18:32 +0000

    Update int128/release-typescript-action@v1.78.0

---
+++ src/semver.ts
@@ -0,0 +1,19 @@
+export type Level = 'minor' | 'patch'
+
+export const computeNextTag = (currentTag: string | undefined, majorTag: string, level: Level): string => {
+  if (currentTag === undefined) {
+    return `${majorTag}.0.0`
+  }
+  // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+  const currentTagMatcher = currentTag.match(/^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)
+  if (!currentTagMatcher) {
+    throw new Error(`current tag ${currentTag} is not in form of v0.0.0`)
+  }
+  const [, , minor, patch] = currentTagMatcher
+  if (level === 'patch') {
+    const patchNumber = parseInt(patch, 10)
+    return `${majorTag}.${minor}.${patchNumber + 1}`
+  }
+  const minorNumber = parseInt(minor, 10)
+  return `${majorTag}.${minorNumber + 1}.0`
+}
